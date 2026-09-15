464c2d2e13fdda3b364afcfe48f42cb34bb0173e
lokendrasingh007 <ls0547925@gmail.com>
2026-09-15 12:22:01 +0530

    refactor: rename NEXT_PUBLIC_BASE_URL to NEXT_API_BASE_URL

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    baseURL: process.env.NEXT_API_BASE_URL,
     timeout: 60000, // 60 seconds — image upload ke liye
     withCredentials: true
 });
